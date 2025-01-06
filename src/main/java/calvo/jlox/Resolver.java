f786063312c08f99cf562ee02c68de84c7bb6fbe
calvo.generico <miguelfeliped@gmail.com>
2025-01-06 09:02:10 -0300

    added resolver

--- src/main/java/calvo/jlox/Resolver.java
+++ src/main/java/calvo/jlox/Resolver.java
@@ -5,9 +5,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+
+
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+  private enum FunctionType {
+    NONE,
+    FUNCTION
+  }
+
   private final Interpreter interpreter;
   private Stack<Map<String, Boolean>> scopes = new Stack<>();
+  private FunctionType currentFunction = FunctionType.NONE;
+
 
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
@@ -101,7 +110,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     declare(stmt.name);
     define(stmt.name);
 
-    resolveFunction(stmt);
+    resolveFunction(stmt, null);
     return null;
   }
 
@@ -121,6 +130,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitReturnStmt(Stmt.Return stmt) {
+    if (currentFunction == FunctionType.NONE) {
+      Lox.error(stmt.keyword, "Can't return from top-level code.");
+    }
+
     if (stmt.value != null) {
       resolve(stmt.value);
     }
@@ -173,7 +186,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     expr.accept(this);
   }
 
-  private void resolveFunction(Stmt.Function function) {
+  private void resolveFunction(Stmt.Function function, FunctionType type) {
+    FunctionType enclosingFunction = currentFunction;
+    currentFunction = type;
+
     beginScope();
     for (Token param : function.params) {
       declare(param);
@@ -181,6 +197,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
     resolve(function.body);
     endScope();
+    currentFunction = enclosingFunction;
   }
 
   private void beginScope() {
